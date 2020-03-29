055990fd977622b864da0a068a7564b39de2144c
Yen Truong <truthblue82@gmail.com>
2020-03-29 23:38:07 +0700

    optimize

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,7 +3,7 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Auxilari from '../../../hoc/Auxilari';
+import Auxilari from '../../../hoc/Auxilari/Auxilari';
 
 const sideDrawer = (props) => {
     let attachedClasses = ["SideDrawer", "Close"];
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
-import Auxilari from '../../../hoc/Auxilari';
+import Auxilari from '../../../hoc/Auxilari/Auxilari';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Auxilari>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div className="Modal" style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1': '0'
-        }}>
-            {props.children}
-        </div>
-    </Auxilari>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate () {
+        console.log('[Modal] will update');
+    }
+    render () {
+        return (
+            <Auxilari>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className="Modal" style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1': '0'
+                }}>
+                    {this.props.children}
+                </div>
+            </Auxilari>
+        );
+    }
+};
+
+export default Modal;
