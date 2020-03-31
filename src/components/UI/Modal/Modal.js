ce3b9758dcc9f7c2c360d1b47cb31330e494f281
Yen Truong <truthblue82@gmail.com>
2020-03-31 18:07:14 +0700

    remove interceptors

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import classes from './Modal.css';
+import './Modal.css';
 import Auxilari from '../../../hoc/Auxilari/Auxilari';
 import Backdrop from '../Backdrop/Backdrop';
 
