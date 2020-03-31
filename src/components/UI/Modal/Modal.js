13677bd997672207a0d67b663f705ab05a972bcd
Yen Truong <truthblue82@gmail.com>
2020-03-31 17:12:24 +0700

    add spinner for purchase

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,8 +4,9 @@ import Auxilari from '../../../hoc/Auxilari/Auxilari';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
+
     shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show;
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
     componentWillUpdate () {
